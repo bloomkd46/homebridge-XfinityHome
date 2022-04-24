55cde2e4268b8faeaaff562c9155679f5552c77c
Kolton Bloom <bloomkd46@icloud.com>
2022-04-24 11:29:45 -0700

    hopefully fixed light flicker in accesories when toggling light

--- src/accessories/LightAccessory.ts
+++ src/accessories/LightAccessory.ts
@@ -48,8 +48,12 @@ export default class LightAccessory extends Accessory {
   }
 
   async set(value: CharacteristicValue): Promise<void> {
+    this.service.updateCharacteristic(this.platform.Characteristic.On, value);
     return new Promise((resolve, reject) => {
-      this.device.set(value as number | boolean).then(() => resolve()).catch(err => {
+      this.device.set(value as number | boolean).then(() => {
+        this.service.updateCharacteristic(this.platform.Characteristic.On, value);
+        resolve();
+      }).catch(err => {
         this.log('error', `Failed To Set ${typeof value === 'number' ? 'Brightness' : 'IsOn'} With Error:\n`, err.response.data);
         reject(new this.StatusError(HAPStatus.SERVICE_COMMUNICATION_FAILURE));
       });
