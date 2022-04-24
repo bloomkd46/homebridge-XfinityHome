651fc19339640a1a3d4b020a6f969b8a494db94a
Kolton Bloom <bloomkd46@icloud.com>
2022-04-24 11:46:37 -0700

    working on duplicate set lof for light

--- src/accessories/LightAccessory.ts
+++ src/accessories/LightAccessory.ts
@@ -48,7 +48,7 @@ export default class LightAccessory extends Accessory {
   }
 
   async set(value: CharacteristicValue): Promise<void> {
-    this.service.updateCharacteristic(this.platform.Characteristic[typeof value === 'boolean' ? 'On' : 'Brightness'], value);
+    //this.service.updateCharacteristic(this.platform.Characteristic[typeof value === 'boolean' ? 'On' : 'Brightness'], value);
     return new Promise((resolve, reject) => {
       this.device.set(value as number | boolean).then(() => {
         this.service.updateCharacteristic(this.platform.Characteristic[typeof value === 'boolean' ? 'On' : 'Brightness'], value);
