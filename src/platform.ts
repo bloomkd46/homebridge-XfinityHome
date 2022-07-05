59001fb2ce7862f31bc80295eee66956923f6eb3
Kolton Bloom <bloomkd46@icloud.com>
2022-07-05 15:59:49 -0700

    Better Logging During Shutdown

--- src/platform.ts
+++ src/platform.ts
@@ -58,6 +58,19 @@ export class XfinityHomePlatform implements DynamicPlatformPlugin {
       // run the method to discover / register your devices as accessories
       this.discoverDevices();
     });
+    this.api.on(APIEvent.DID_FINISH_LAUNCHING, () => {
+      const projectDir = path.join(api.user.storagePath(), 'XfinityHome');
+      const generalLogPath = path.join(projectDir, 'General.log');
+      if (!fs.existsSync(projectDir)) {
+        fs.mkdirSync(projectDir);
+      }
+      const date = new Date();
+      const time = `${('0' + (date.getMonth() + 1)).slice(-2)}/${('0' + date.getDate()).slice(-2)}/${date.getFullYear()}, ` +
+        `${('0' + (date.getHours() % 12)).slice(-2)}:${('0' + (date.getMinutes())).slice(-2)}:${('0' + (date.getSeconds())).slice(-2)} ` +
+        `${date.getHours() > 12 ? 'PM' : 'AM'}`;
+      fs.appendFileSync(generalLogPath, `[${time}] Server Stopped\n`);
+
+    })
   }
 
   /**
