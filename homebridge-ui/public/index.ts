f622f939fdec54f8e58bda9ecd12b30b4683bc75
Kolton Bloom <bloomkd46@icloud.com>
2022-12-22 11:19:36 -0800

    added custom ui typescript files

---
+++ homebridge-ui/public/index.ts
@@ -0,0 +1,236 @@
+/* eslint-disable no-console */
+import type { IHomebridgePluginUi } from '@homebridge/plugin-ui-utils/dist/ui.interface';
+declare const homebridge: IHomebridgePluginUi;
+
+//Intro Elements
+const pageIntro = document.getElementById('pageIntro') as HTMLDivElement;
+const introContinue = document.getElementById('introContinue') as HTMLButtonElement;
+//Settings Elements
+const menuSettings = document.getElementById('menuSettings') as HTMLButtonElement;
+//Config Tool Elements
+const menuConfigTool = document.getElementById('menuConfigTool') as HTMLButtonElement;
+const pageConfigTool = document.getElementById('pageConfigTool') as HTMLDivElement;
+const exitConfigTool = document.getElementById('exitSetup') as HTMLButtonElement;
+//  Part 1
+const configToolPart1 = document.getElementById('part1') as HTMLDivElement;
+const configToolQRCodeAlt = document.getElementById('qrcode-alt') as HTMLSpanElement;
+const configToolCertificate = document.getElementById('certificate') as HTMLAnchorElement;
+const configToolQRCodeSvg = document.getElementById('qrcode-svg') as HTMLElement;
+const configToolQRCode = document.getElementById('qrcode') as HTMLSpanElement;
+const configToolIP = document.getElementById('ip') as HTMLSpanElement;
+const configToolPort = document.getElementById('port') as HTMLSpanElement;
+//  Part 2
+const configToolPart2 = document.getElementById('part2') as HTMLDivElement;
+//  Part 3
+const configToolPart3 = document.getElementById('part3') as HTMLDivElement;
+//Log Elements
+const menuLogs = document.getElementById('menuLogs') as HTMLButtonElement;
+const pageLogs = document.getElementById('pageLogs') as HTMLDivElement;
+const deviceSelect = document.getElementById('deviceSelect') as HTMLSelectElement;
+const logDownload = document.getElementById('logDownload') as HTMLAnchorElement;
+const logDelete = document.getElementById('logDelete') as HTMLButtonElement;
+const logZone = document.getElementById('logZone') as HTMLDivElement;
+//Device Info Elements
+const deviceInfo = document.getElementById('deviceInfo') as HTMLButtonElement;
+const deviceName = document.getElementById('deviceName') as HTMLHeadingElement;
+const deviceDetailsTable = document.getElementById('deviceDetailsTable') as HTMLTableSectionElement;
+//Miscellaneous Elements
+const menuWrapper = document.getElementById('menuWrapper') as HTMLDivElement;
+(async () => {
+  try {
+    let currentLogPath = '';
+    let currentLog = '';
+    const currentConfig = await homebridge.getPluginConfig();
+    const showIntro = () => {
+      introContinue.addEventListener('click', () => {
+        homebridge.showSpinner();
+        pageIntro.style.display = 'none';
+        menuWrapper.style.display = 'inline-flex';
+        showConfigTool();
+      });
+      pageIntro.style.display = 'block';
+    };
+    const showLogs = async () => {
+      homebridge.showSpinner();
+      homebridge.hideSchemaForm();
+      menuConfigTool.classList.remove('btn-elegant');
+      menuConfigTool.classList.add('btn-primary');
+      menuLogs.classList.add('btn-elegant');
+      menuLogs.classList.remove('btn-primary');
+      menuSettings.classList.remove('btn-elegant');
+      menuSettings.classList.add('btn-primary');
+      pageConfigTool.style.display = 'none';
+      pageLogs.style.display = 'block';
+      let cachedAccessories = await homebridge.request('/getCachedAccessories');
+      if (cachedAccessories.length > 0) {
+        cachedAccessories.sort((a, b) => {
+          return a.displayName.toLowerCase() > b.displayName.toLowerCase()
+            ? 1
+            : b.displayName.toLowerCase() > a.displayName.toLowerCase()
+              ? -1
+              : 0;
+        });
+      }
+      deviceSelect.innerHTML = '';
+      cachedAccessories.forEach(a => {
+        if (a.context.logPath) {
+          const option = document.createElement('option');
+          option.text = a.displayName;
+          option.value = a.context.logPath;
+          deviceSelect.add(option);
+        }
+      });
+      const showDeviceLog = async logPath => {
+        currentLogPath = logPath;
+        homebridge.showSpinner();
+        const logs = await homebridge.request('/getLogs', { logPath: logPath });
+        currentLog = `data:text/plain;base64,${btoa(logs.split('<br>').join('\n'))}`;
+        logDownload.href = currentLog;
+        logDownload.download = logPath.split('/').pop();
+
+        logZone.innerHTML = logs;
+        logZone.scrollTo(0, logZone.scrollHeight);
+
+        const device = cachedAccessories.find(x => x.context.logPath === logPath);
+        if (device) {
+          deviceInfo.style.display = 'inline';
+          deviceName.innerHTML = device.displayName;
+          let deviceHTML = '';
+          Object.keys(device.context.device).forEach(key => {
+            deviceHTML +=
+              `<tr>
+                    <th scope="row">${key}</th>
+                    <td><pre style="color: inherit;">${JSON.stringify(device.context.device[key], null, 1)}</pre></td>
+                  </tr>`;
+          });
+          deviceDetailsTable.innerHTML = deviceHTML;
+        } else {
+          deviceInfo.style.display = 'none';
+        }
+
+        homebridge.hideSpinner();
+
+        homebridge.request('/watchForChanges', { path: logPath }).then(() => {
+          if (logPath === currentLogPath) {
+            showDeviceLog(logPath);
+            setTimeout(async () => {
+              if (logPath === currentLogPath) {
+                cachedAccessories = await homebridge.request('/getCachedAccessories');
+
+                showDeviceLog(logPath);
+              }
+            }, 1500);
+          }
+        }).catch(err => {
+          console.error(err);
+        });
+      };
+      deviceSelect.addEventListener('change', () => showDeviceLog(deviceSelect.value));
+      if (cachedAccessories.length > 0) {
+        const generalLog = await homebridge.request('/getGeneralLog');
+        const option = document.createElement('option');
+        option.text = 'General';
+        option.value = generalLog;
+        option.selected = true;
+        deviceSelect.add(option, 0);
+        showDeviceLog(generalLog);
+      } else {
+        const option = document.createElement('option');
+        option.text = 'No Devices';
+        deviceSelect.add(option);
+        deviceSelect.disabled = true;
+      }
+      homebridge.hideSpinner();
+    };
+    const showConfigTool = async () => {
+      homebridge.showSpinner();
+      menuWrapper.style.display = 'none';
+      homebridge.hideSchemaForm();
+      menuConfigTool.classList.add('btn-elegant');
+      menuConfigTool.classList.remove('btn-primary');
+      menuLogs.classList.remove('btn-elegant');
+      menuLogs.classList.add('btn-primary');
+      menuSettings.classList.remove('btn-elegant');
+      menuSettings.classList.add('btn-primary');
+      pageConfigTool.style.display = 'block';
+      pageLogs.style.display = 'none';
+
+      configToolPart1.style.display = 'block';
+      configToolPart2.style.display = 'none';
+      configToolPart3.style.display = 'none';
+
+      const proxy = await homebridge.request('/startProxy');
+      configToolIP.innerText = proxy.ip;
+      configToolPort.innerText = proxy.port;
+      configToolQRCodeAlt.style.display = 'inline';
+      const certificateLink = `http://${proxy.ip}:${proxy.port}/cert`;
+      configToolCertificate.innerText = certificateLink;
+      configToolCertificate.href = certificateLink;
+      configToolQRCodeSvg.innerHTML = proxy.qrcode;
+      configToolQRCode.style.display = 'none';
+      if (!(/iPad|iPhone|iPod/.test(navigator.userAgent))) {
+        configToolQRCode.style.display = 'block';
+        configToolQRCodeAlt.style.display = 'none';
+      }
+      configToolPart1.style.display = 'block';
+      homebridge.hideSpinner();
+
+      await homebridge.request('/proxyActive');
+      homebridge.showSpinner();
+      configToolPart1.style.display = 'none';
+      configToolPart2.style.display = 'block';
+      homebridge.hideSpinner();
+
+      const token = await homebridge.request('/token');
+      homebridge.showSpinner();
+      configToolPart2.style.display = 'none';
+      configToolPart3.style.display = 'block';
+      currentConfig[0].refreshToken = token;
+      await homebridge.updatePluginConfig(currentConfig);
+      await homebridge.savePluginConfig();
+      homebridge.hideSpinner();
+    };
+    const showSettings = async () => {
+      homebridge.showSpinner();
+      menuConfigTool.classList.remove('btn-elegant');
+      menuConfigTool.classList.add('btn-primary');
+      menuLogs.classList.remove('btn-elegant');
+      menuLogs.classList.add('btn-primary');
+      menuSettings.classList.add('btn-elegant');
+      menuSettings.classList.remove('btn-primary');
+      pageConfigTool.style.display = 'none';
+      pageLogs.style.display = 'none';
+      homebridge.showSchemaForm();
+      homebridge.hideSpinner();
+    };
+    menuSettings.addEventListener('click', () => showSettings());
+    menuLogs.addEventListener('click', () => showLogs());
+    menuConfigTool.addEventListener('click', () => showConfigTool());
+    exitConfigTool.addEventListener('click', async () => {
+      await homebridge.request('/stopProxy');
+      menuWrapper.style.display = 'inline-flex';
+      showSettings();
+    });
+    logDelete.addEventListener('click', async () => {
+      homebridge.showSpinner();
+      logZone.innerHTML = 'Deleting log file at ' + currentLogPath;
+      await homebridge.request('/deleteLog', { logPath: currentLogPath });
+      logZone.innerHTML = 'successfully deleted log file at ' + currentLogPath;
+      homebridge.hideSpinner();
+    });
+    if (currentConfig.length) {
+      menuWrapper.style.display = 'inline-flex';
+      showSettings();
+    } else {
+      currentConfig.push({ name: 'Xfinity Home' });
+      await homebridge.updatePluginConfig(currentConfig);
+      showIntro();
+    }
+  } catch (err: any) {
+    homebridge.toast.error(err.message, 'Error');
+    console.log(err);
+    homebridge.closeSettings();
+  } finally {
+    homebridge.hideSpinner();
+  }
+})();
---
+++ homebridge-ui/server.ts
@@ -0,0 +1,258 @@
+/* eslint-disable no-console */
+import debug from 'debug';
+import { EventEmitter } from 'events';
+import { existsSync, mkdirSync, readFileSync, rmSync, statSync, unwatchFile, watch, watchFile } from 'fs';
+import Proxy from 'http-mitm-proxy';
+import os from 'os';
+import path from 'path';
+import qrcode from 'qrcode';
+
+import { HomebridgePluginUiServer } from '@homebridge/plugin-ui-utils';
+
+
+class PluginUiServer extends HomebridgePluginUiServer {
+  constructor() {
+    const events = new EventEmitter();
+    super();
+
+    const plugin = 'homebridge-xfinityhome';
+    const platform = 'XfinityHomePlatform';
+    const storagePath = this.homebridgeStoragePath ?? '';
+    const configPath = this.homebridgeConfigPath ?? '';
+    const config = JSON.parse(readFileSync(configPath, 'utf-8')).platforms.find((plugin) => plugin.platform === platform);
+
+    /*
+      A native method getCachedAccessories() was introduced in config-ui-x v4.37.0
+      The following is for users who have a lower version of config-ui-x
+    */
+    let cachedAccessoriesDir;
+    this.onRequest('/getCachedAccessories', async () => {
+      try {
+        // Define the plugin and create the array to return
+        if (cachedAccessoriesDir && existsSync(cachedAccessoriesDir)) {
+          return JSON.parse(readFileSync(cachedAccessoriesDir, 'utf-8')).filter(accessory => accessory.plugin === plugin);
+        } else if (!cachedAccessoriesDir) {
+          cachedAccessoriesDir = path.join(storagePath, '/accessories/cachedAccessories') +
+            (config._bridge?.username ? ('.' + config._bridge?.username?.split(':').join('')) : '');
+          return JSON.parse(readFileSync(cachedAccessoriesDir, 'utf-8')).filter(accessory => accessory.plugin === plugin);
+        } else {
+          return [];
+        }
+      } catch (err) {
+        // Just return an empty accessory list in case of any errors
+        console.log(err);
+        return [];
+      }
+    });
+    this.onRequest('/getGeneralLog', async () => {
+      return path.join(storagePath, 'XfinityHome', 'General.log');
+    });
+    this.onRequest('/getLogs', async (payload) => {
+      try {
+        return readFileSync(payload.logPath).toString().split('\n').join('<br>');
+      } catch (err) {
+        return `Failed To Load Logs From ${payload.logPath}`;
+      }
+    });
+    this.onRequest('/getRelativePath', (payload) => {
+      return path.relative(path.join(__dirname, '/public/index.html'), payload.path);
+    });
+    this.onRequest('/deleteLog', async (payload) => {
+      try {
+        return rmSync(payload.logPath, { force: true });
+      } catch (err) {
+        return err;
+      }
+    });
+    this.onRequest('/watchForChanges', async (payload) => {
+      return new Promise((resolve, reject) => {
+        if (!existsSync(payload.path)) {
+          reject('File does not exist: ' + payload.path);
+          return;
+        }
+        try {
+          const aborter = new AbortController();
+          const watcher = watch(payload.path, { signal: aborter.signal });
+          watcher.once('change', () => {
+            aborter.abort();
+            resolve('');
+          });
+          watcher.once('error', err => {
+            console.error(err);
+            aborter.abort();
+            watchFile(payload.path, () => {
+              unwatchFile(payload.path);
+              resolve('');
+            });
+          });
+        } catch {
+          watchFile(payload.path, () => {
+            unwatchFile(payload.path);
+            resolve('');
+          });
+        }
+      });
+    });
+
+    this.onRequest('/proxyActive', async () => {
+      return new Promise((resolve) => {
+        events.on('proxy', () => resolve(''));
+      });
+    });
+    /*this.onRequest('/sslActive', async () => {
+      return new Promise((resolve) => {
+        events.on('ssl', () => resolve());
+      });
+    });*/
+    this.onRequest('/token', async () => {
+      return new Promise((resolve) => {
+        events.on('token', token => resolve(token));
+      });
+    });
+
+    this.onRequest('/startProxy', async () => {
+      // Disable debug messages from the proxy
+      try {
+        debug.disable();
+      } catch (err) {
+        //Do nothing
+      }
+      const ROOT = path.join(storagePath, 'XfinityHome');
+      if (!existsSync(ROOT)) {
+        mkdirSync(ROOT);
+      }
+
+      const pemFile = path.join(ROOT, 'certs', 'ca.pem');
+
+      const localIPs: string[] = [];
+      const ifaces = os.networkInterfaces();
+      Object.keys(ifaces).forEach(name => {
+        ifaces[name]?.forEach(network => {
+          const familyV4Value = typeof network.family === 'string' ? 'IPv4' : 4;
+          if (network.family === familyV4Value && !network.internal) {
+            localIPs.push(network.address);
+          }
+        });
+      });
+      localIPs.push(os.hostname() + os.hostname().endsWith('.local') ? '' : '.local');
+
+
+
+      const proxy = Proxy();
+      const localIPPorts = localIPs.map(ip => `${ip}:${585}`);
+
+      proxy.onError((ctx, err) => {
+        switch (err?.name) {
+          case 'ERR_STREAM_DESTROYED':
+          case 'ECONNRESET':
+            return;
+
+          case 'ECONNREFUSED':
+            console.error('Failed to intercept secure communications. This could happen due to bad CA certificate.');
+            return;
+
+          case 'EACCES':
+            console.error(`Permission was denied to use port ${585}.`);
+            return;
+
+          default:
+          //console.error('Error:', err.code, err);
+        }
+      });
+
+      proxy.onRequest((ctx, callback) => {
+        if (ctx.clientToProxyRequest.method === 'GET' && ctx.clientToProxyRequest.url === '/cert' &&
+          localIPPorts.includes(ctx.clientToProxyRequest.headers.host ?? '')) {
+          ctx.use(Proxy.gunzip);
+          console.log('Intercepted certificate request');
+
+          ctx.proxyToClientResponse.writeHead(200, {
+            'Accept-Ranges': 'bytes',
+            'Cache-Control': 'public, max-age=0',
+            'Content-Type': 'application/x-x509-ca-cert',
+            'Content-Disposition': 'attachment; filename=cert.pem',
+            'Content-Transfer-Encoding': 'binary',
+            'Content-Length': statSync(pemFile).size,
+            'Connection': 'keep-alive',
+          });
+          //ctx.proxyToClientResponse.end(fs.readFileSync(path.join(ROOT, 'certs', 'ca.pem')));
+          ctx.proxyToClientResponse.write(readFileSync(pemFile));
+          ctx.proxyToClientResponse.end();
+
+          return;
+
+        } else if (ctx.clientToProxyRequest.method === 'POST' && ctx.clientToProxyRequest.headers.host === 'oauth.xfinity.com' &&
+          ctx.clientToProxyRequest.url === '/oauth/token') {
+          ctx.use(Proxy.gunzip);
+
+          ctx.onRequestData((ctx, chunk, callback) => {
+            return callback(undefined, chunk);
+          });
+          ctx.onRequestEnd((ctx, callback) => {
+            callback();
+          });
+
+          const chunks: Buffer[] = [];
+          ctx.onResponseData((ctx, chunk, callback) => {
+            chunks.push(chunk);
+            return callback(undefined, chunk);
+          });
+          ctx.onResponseEnd((ctx, callback) => {
+            events.emit('token', JSON.parse(Buffer.concat(chunks).toString()).refresh_token);
+            //token = JSON.parse(Buffer.concat(chunks).toString()).refresh_token;
+            //this.pushEvent('token', { refreshToken: JSON.parse(Buffer.concat(chunks).toString()).refresh_token });
+            //emitter.emit('tuya-config', Buffer.concat(chunks).toString());
+            callback();
+          });
+        } else {
+          //this.pushEvent('proxy', {});
+          events.emit('proxy');
+          /*ctx.onRequestData(function (ctx, chunk, callback) {
+            ctx.onResponseData(function (ctx, chunk, callback) {
+              //this.pushEvent('sslProxy', {});
+              events.emit('ssl');
+            });
+          });*/
+        }
+        return callback();
+      });
+
+      /*emitter.on('tuya-config', body => {
+        //if (body.indexOf('tuya.m.my.group.device.list') === -1) return;
+        console.log('Intercepted token from Xfinity Home');
+        try {
+          console.log('Your refresh token is: ' + JSON.parse(body).refresh_token);
+        } catch (err) {
+          console.error(err);
+        }
+      });*/
+      this.onRequest('/stopProxy', () => {
+        if (proxy && typeof proxy.close === 'function') {
+          proxy.close();
+        }
+        if (existsSync(path.join(ROOT, 'certs'))) {
+          rmSync(path.join(ROOT, 'certs'), { recursive: true, force: true });
+        }
+        if (existsSync(path.join(ROOT, 'keys'))) {
+          rmSync(path.join(ROOT, 'keys'), { recursive: true, force: true });
+        }
+        return '';
+      });
+      return new Promise((resolve) => {
+        proxy.listen({ port: 585, sslCaDir: ROOT }, async err => {
+          if (err) {
+            console.error('Error starting proxy: ' + err);
+          }
+          const address = localIPs[0];
+          const port = 585;
+          qrcode.toString(`http://${address}:${port}/cert`, { type: 'svg' })
+            .then(qrcode => resolve({ ip: address, port: port, qrcode: qrcode }));
+        });
+      });
+
+    });
+    this.ready();
+  }
+}
+
+(() => new PluginUiServer())();
